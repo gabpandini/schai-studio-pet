3e840242c586c33ba4c68ca5f555b1849b36395d
gabriela.pandini <gabriela.pandini@ambevtech.com.br>
2022-03-18 16:35:03 -0300

    Estilizando página Sobre

--- src/components/sobre/Sobre.tsx
+++ src/components/sobre/Sobre.tsx
@@ -1,20 +1,25 @@
 import React from "react"
 import "./sobre.css"
+import Whats from "../../img/whats.png"
+import Instagram from "../../img/insta.png"
 
 function Sobre () {
     return(
         <div className="Sobre" >
-          <div>
+          <div className="Sobre__endereco">
             <h2 className="Sobre__titulos" >Aonde estamos:</h2>
-            <p>*endereço aqui*</p>
+            <p>Rua Theodoro Holtrup, 504 - Sala 10 - Vila Nova</p>
+            <p>Blumenau/SC, 89035-300</p>
           </div>
-          <div>
+          <div className="Sobre__redesSociais">
             <h2 className="Sobre__titulos" >Acompanhe-nos em nossas redes socias!</h2>
-            <span>*redes sociais aqui*</span>
+            <a href="https://www.instagram.com/schai.studiopet/"><img className="Sobre__img" src={Instagram} alt="Link para o Instagram" /></a>
           </div>
-          <div>
-            <h2 className="Sobre__titulos" >Ou fale conosco pelo WhatsApp</h2>
-            <span>*zap aqui*</span>
+          <div className="Sobre__whats">
+            <h2 className="Sobre__titulos" >Ou fale conosco pelo WhatsApp:</h2>
+            <a href="https://api.whatsapp.com/message/YPDMFML2QSPZB1?autoload=1&app_absent=0">
+              <img className="Sobre__img" src={Whats} alt="Link para o WhatsApp" />
+            </a>
           </div>
         </div>
     )
